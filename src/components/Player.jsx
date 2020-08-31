e84066dbfaa463329692c99390239dd2dfd9e03b
Dharmang92 <dharmang.gajjar92@gmail.com>
2020-08-31 13:49:48 +0530

    trying to play song from the server - failed

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,7 +4,11 @@ function Player({ name }) {
     return (
         <div className="bg-red-400 md:w-3/12 md:float-left">
             {name && <div>Now Playing: {name}</div>}
-            <audio controls autoPlay src=""></audio>
+            <audio
+                controls
+                autoPlay
+                src="https://assets.coderrocketfuel.com/pomodoro-times-up.mp3"
+            ></audio>
         </div>
     );
 }
